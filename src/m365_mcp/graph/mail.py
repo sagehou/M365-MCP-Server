70a010f0644e5b8094eecc40879551c303b6655d
Sage Hou <sagehou@outlook.com>
2026-09-17 11:21:53 +0800

    Normalize mail search query

--- src/m365_mcp/graph/mail.py
+++ src/m365_mcp/graph/mail.py
@@ -46,7 +46,7 @@ class MailService:
             "$orderby": "receivedDateTime desc",
         }
         headers: dict[str, str] = {}
-        search_text = query.strip().replace('"', " ")
+        search_text = query.replace('"', " ")
         if search_text:
             params["$search"] = f'"{search_text}"'
             headers["ConsistencyLevel"] = "eventual"
